1c287a3f994e6ace65255558d83ee61f6f6f457c
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-17 14:09:59 -0500

    Add files via upload

---
+++ routes/rutas.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import httpRuta from "../controllers/ruta.js";
+import { check } from "express-validator"
+
+const router = new Router();
+
+router.get('/hola', httpRuta.getrutas);
+
+router.get('/ruta/:id',[
+  check("id", "Digite el id").not().isEmpty(),
+  check("id", "Digite el id").isMongoId(),
+  validarCampos
+],httpRuta.getRutaId);
+
+router.post('/agregar',[
+  check("precio","Digite su precio").not().isEmpty(),
+  check("origen","Digite su origen").not().isEmpty(),
+  check("destino","Digite su destino").not().isEmpty(),
+  validarCampos
+],httpRuta.postRutas);
+
+router.put('/ruta/:id', [
+  check("id", "Digite el id").not().isEmpty(),
+  check("id", "Digite el id").isMongoId(),
+  check("id","Digite su id").not().isEmpty(),
+  check("precio","Digite su precio").not().isEmpty(),
+  check("origen","Digite su origen").not().isEmpty(),
+  check("destino","Digite su destino").not().isEmpty(),
+  validarCampos
+],httpRuta.putRutas);
+
+router.delete('/ruta/:id',[
+  check("id", "Digite el id").not().isEmpty(),
+  check("id", "Digite el id").isMongoId(),
+  validarCampos
+],httpRuta.getRutaId);
+
+router.put('inactivarRuta/:id',[],httpRuta.putRutaInactivar)
+  check("id", "Digite el id").not().isEmpty(),
+  check("id", "Digite el id").isMongoId(),
+  validarCampos
+router.put('activarRuta/:id',[
+  check("id", "Digite el id").not().isEmpty(),
+  check("id", "Digite el id").isMongoId(),
+  validarCampos
+],httpRuta.putRutaActivar)
+
+
+export default router;
