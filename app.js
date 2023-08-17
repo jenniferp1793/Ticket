b2b6ec317f5463ae64c2e38eabe13b04cd08d997
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-17 14:10:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-import express from "express";
-import 'dotenv/config'
-import clientes  from "./routes/clientes.js";
+import  express  from "express";
+import mongoose from 'mongoose';
+import "dotenv/config"
+import bus from "./routes/bus.js"
+import cliente from "./routes/clientes.js"
+import conductor from "./routes/conductor.js";
+import horario from "./routes/horario.js";
+import ruta from "./routes/rutas.js"
+import ticket from "./routes/ticket.js"
+import vendedor from "./routes/vendedores.js"
 
 const app = express()
-app.use(express.json())
-app.use(   "/api/clientes"   ,clientes)
+app.use(express.json()) 
 
+app.use("/api/bus", bus)
+app.use("/api/clientes", cliente)
+app.use("/api/conductor", conductor)
+app.use("/api/horario", horario)
+app.use("/api/rutas", ruta)
+app.use("/api/ticket", ticket)
+app.use("/api/vendedores", vendedor)
 
 app.listen(process.env.PORT,()=>{
-  console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
-  
-})
+    mongoose.connect(process.env.mongoDB)
+    .then(() => console.log('Conectado!'));
+    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+  })
