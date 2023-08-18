1a373fa79f46cb3baae2a829f5c2afccf0cab817
jenniferp1793 <131495330+jenniferp1793@users.noreply.github.com>
2023-08-18 13:22:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ import  express  from "express";
 import mongoose from 'mongoose';
 import "dotenv/config"
 import bus from "./routes/bus.js"
-import cliente from "./routes/clientes.js"
+import cliente from "./routes/cliente.js"
 import conductor from "./routes/conductor.js";
 import horario from "./routes/horario.js";
-import ruta from "./routes/rutas.js"
+import ruta from "./routes/ruta.js"
 import ticket from "./routes/ticket.js"
-import vendedor from "./routes/vendedores.js"
+import vendedor from "./routes/vendedor.js"
 
 const app = express()
 app.use(express.json()) 
 
 app.use("/api/bus", bus)
-app.use("/api/clientes", cliente)
+app.use("/api/cliente", cliente)
 app.use("/api/conductor", conductor)
 app.use("/api/horario", horario)
-app.use("/api/rutas", ruta)
+app.use("/api/ruta", ruta)
 app.use("/api/ticket", ticket)
-app.use("/api/vendedores", vendedor)
+app.use("/api/vendedor", vendedor)
 
 app.listen(process.env.PORT,()=>{
     mongoose.connect(process.env.mongoDB)
     .then(() => console.log('Conectado!'));
     console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
-  })
+})
